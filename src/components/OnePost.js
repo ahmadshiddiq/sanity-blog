e4954853c0cd2afdbfe82a5116355ba6d94ed869
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2020-12-19 22:33:54 +0700

    feat: Finish following tutorial

---
+++ src/components/OnePost.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import sanityClient from "../client";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
+
+export default function AllPosts() {
+  const [postData, setPostData] = useState(null);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[slug.current == $slug]{
+        title,
+        slug,
+        mainImage{
+          asset->{
+            _id,
+            url
+          }
+        },
+        body,
+        "authorName": author->name,
+        "authorImage": author->image
+      }`,
+        { slug }
+      )
+      .then((data) => setPostData(data[0]))
+      .catch(console.error);
+  }, [slug]);
+
+  if (!postData) return <div>Loading ...</div>;
+
+  return (
+    <div className="bg-gray-200 min-h-screen p-12">
+      <div className="container shadow-lg mx-auto bg-green-100 rounded-lg">
+        <div className="relative">
+          <div className="absolute h-full w-full flex items-center justify-center p-8">
+            <div className="bg-white bg-opacity-75 p-12">
+              <h2 className="text-3xl lg:text-6xl mb-4">{postData.title}</h2>
+              <div className="flex justify-center text-gray-800">
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src={urlFor(postData.authorImage).width(100).url()}
+                  alt="Author is Ahmad Shiddiq"
+                />
+                <h4 className="flex items-center pl-2 text-2xl">
+                  {postData.authorName}
+                </h4>
+              </div>
+            </div>
+          </div>
+          <img
+            className="w-full object-cover rounded-t"
+            style={{ height: "400px" }}
+            src={urlFor(postData.mainImage).width(200).url()}
+            alt=""
+          />
+        </div>
+        <div className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full">
+          <BlockContent
+            blocks={postData.body}
+            projectId={sanityClient.clientConfig.projectId}
+            dataset={sanityClient.clientConfig.dataset}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
